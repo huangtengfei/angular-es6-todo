931759c027dc214cae26a997867cfa3f45dfa55d
HTF <457296373@qq.com>
2015-10-28 20:36:53 +0800

    update

--- src/scripts/controllers/HomeController.js
+++ src/scripts/controllers/HomeController.js
@@ -8,10 +8,10 @@ export default class HomeController {
 	  will annotate the constructor after compiling for minification.
 	*/
 	/*@ngInject*/
-	constructor(TodoService) {
+	constructor($scope, $filter, TodoService) {
 
 		var vm = this;	// the this keyword is contextual, use a capture variable for this
-		
+
 		vm.service = TodoService;
 
 	    (() => {
@@ -21,15 +21,34 @@ export default class HomeController {
 			    });
 	    })();
 
-	    vm.addTodo = () => {
+	    // use $scope just for use $watch
+	    $scope.$watch('vm.todos', () => {
+	    	vm.uncompleted = $filter('filter')(vm.todos, {completed: false}).length;
+	    }, true);
+
+	    vm.add = () => {
 	    	let todo = {
 				title: vm.newTodo,
 				completed: false
 			};
-			vm.service.save(todo)
-				.then(function(){
+			vm.service.create(todo)
+				.then(() => {
 					vm.newTodo = '';
 				});
+		};
+
+		vm.toggleCompleted = (item, index) => {
+			vm.service.update(item, index).then(() => {}, () => {
+				item.completed = !item.completed;
+			});
+		};
+
+		vm.remove = (index) => {
+			vm.service.delete(index);
+		};
+
+		vm.clearCompleted = () => {
+			vm.service.clear();
 		};
 	}
 }
--- src/scripts/services/TodoService.js
+++ src/scripts/services/TodoService.js
@@ -25,10 +25,50 @@ export default class todoLocService {
 		return deferred.promise;
 	}
 
-	save(item) {
+	create(item) {
 		let deferred = this._$q.defer();
 
 		this.items.push(item);
+
+		this._saveToLocalStorage(this.items);
+		deferred.resolve(this.items);
+
+		return deferred.promise;
+	}
+
+	update(item, index){
+		let deferred = this._$q.defer();
+
+		this.items[index] = item;
+
+		this._saveToLocalStorage(this.items);
+		deferred.resolve(this.items);
+
+		return deferred.promise;
+	}
+
+	delete(index){
+		let deferred = this._$q.defer();
+
+		this.items.splice(index, 1);
+
+		this._saveToLocalStorage(this.items);
+		deferred.resolve(this.items);
+
+		return deferred.promise;
+	}
+
+	clear(){
+		let deferred = this._$q.defer();
+		let uncompleted = [];
+
+		this.items.forEach((item) => {
+			if(!item.completed){
+				uncompleted.push(item);
+			}
+		});
+
+		angular.copy(uncompleted, this.items);
 
 		this._saveToLocalStorage(this.items);
 		deferred.resolve(this.items);
