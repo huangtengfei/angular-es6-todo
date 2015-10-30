2ee6c4124c8b8f8f325946a66e85509b30825b85
HTF <457296373@qq.com>
2015-10-30 10:48:48 +0800

    update controller structure

--- src/scripts/controllers/HomeController.js
+++ src/scripts/controllers/HomeController.js
@@ -10,59 +10,61 @@ export default class HomeController {
 	/*@ngInject*/
 	constructor($scope, $filter, TodoService) {
 
-		var vm = this;	// the this keyword is contextual, use a capture variable for this
-
-		vm.service = TodoService;
+		this.service = TodoService;
+		this.todos = [];
+		this.uncompleted = 0;
 
 	    (() => {
-	    	vm.service.list()
+	    	this.service.list()
 		    	.then((result) => {
-			    	vm.todos = result;			   
+			    	this.todos = result;			   
 			    });
 	    })();
 
 	    // use $scope just for use $watch
 	    $scope.$watch('vm.todos', () => {
-	    	vm.uncompleted = $filter('filter')(vm.todos, {completed: false}).length;
-	    }, true);
-
-	    vm.add = () => {
-	    	if(!vm.newTodo){
-	    		return;
-	    	}
-	    	let todo = {
-				title: vm.newTodo,
-				completed: false
-			};
-			vm.pending = true;
-			vm.service.create(todo)
-				.then(() => {
-					vm.newTodo = '';
-				})
-				.finally(() => {
-					vm.pending = false;
-				});
-		};
-
-		vm.toggleCompleted = (item, index) => {
-			vm.service.update(item, index).then(() => {}, () => {
-				item.completed = !item.completed;
-			});
-		};
-
-		vm.remove = (index) => {
-			vm.service.delete(index);
-		};
-
-		vm.clearCompleted = () => {
-			vm.service.clear();
-		};
-
-		// use status change class, use statusFilter filter todos in the view 
-		vm.filterTodo = (status) => {
-			vm.status = (status === 1) ? 'all' : (status === 2) ? 'active' : 'completed';
-			vm.statusFilter = (status === 1) ? 
-					{} : (status === 2) ? {completed: false} : {completed: true};
-		};
+	    	this.uncompleted = $filter('filter')(this.todos, {completed: false}).length;
+	    }, true);	  
+		
 	}
+
+	toggleCompleted(item) {
+		this.service.update(item, this.todos.indexOf(item)).then(() => {}, () => {
+			item.completed = !item.completed;
+		});
+	}
+
+	add() {
+    	if(!this.newTodo){
+    		return;
+    	}
+    	let todo = {
+			title: this.newTodo,
+			completed: false
+		};
+		this.pending = true;
+		this.service.create(todo)
+			.then(() => {
+				this.newTodo = '';
+			})
+			.finally(() => {
+				this.pending = false;
+			});
+	}
+
+	remove(item) {
+		this.service.delete(this.todos.indexOf(item));
+	}
+
+	clearCompleted() {
+		this.service.clear();
+	}
+
+	// use status change selected style, use statusFilter filter todos in the view 
+	filterTodo(status) {
+		this.status = (status === 1) ? 'all' : (status === 2) ? 'active' : 'completed';
+		this.statusFilter = (status === 1) ? 
+				{} : (status === 2) ? {completed: false} : {completed: true};
+	}
+
 }
